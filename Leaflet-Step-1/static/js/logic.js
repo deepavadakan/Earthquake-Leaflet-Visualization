8fe017a8e4147d922846fa11e11271007d6a440a
Deepa <deepa.vadakan@gmail.com>
2021-01-15 16:15:21 -0500

    part 1

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,13 +1,10 @@
-// Creating our initial map object
-// We set the longitude, latitude, and the starting zoom level
-// This gets inserted into the div with an id of 'mapid'
+// Creating initial map object
 var myMap = L.map("mapid", {
   center: [40.758701, -111.876183],
     zoom: 5
   });
   
-  // Adding a tile layer (the background map image) to our map
-  // We use the addTo method to add objects to our map
+  // Adding a tile layer (the background map image) to the map
   L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -17,50 +14,50 @@ var myMap = L.map("mapid", {
     accessToken: API_KEY
   }).addTo(myMap);
 
+// url with geojson data
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
   
-console.log(url);
+// console.log(url);
 
+// set the colors and limits for Earthquake depth 
 var colors = ['#FFEDA0', '#FED976', '#FEB24C', '#FD8D3C', '#FC4E2A', '#E31A1C', '#BD0026', '#800026']
 var limits = [-10, 0, 5, 10, 15, 20, 25, 30];
 
+// function to find the color given the Earthquake depth
 function getColor(d) {
-  
   for (var i = 0; i < limits.length; i++) {
     if (d < limits[i]) {
-      // console.log(d);
-      // console.log(colors[i]);
       return colors[i];
     }
-    
   }
 }
 
 // Grab the data with d3
-d3.json(url, function(earthquakeData) {
-  console.log(earthquakeData);
+d3.json(url, function(response) {
+  console.log(response);
 
-  var response = earthquakeData.features;
+  var earthquakeData = response.features;
 
   // Loop through data
-  for (var i = 0; i < response.length; i++) {
+  for (var i = 0; i < earthquakeData.length; i++) {
 
     // Set the data location property to a variable
-    var location = response[i].geometry;
-    //console.log(location);
-    //console.log(response[i]);
+    var location = earthquakeData[i].geometry;
+
+    //console.log(earthquakeData[i]);
     if (location) {
       L.circle([location.coordinates[1], location.coordinates[0]], {
         fillOpacity: 0.75,
         color: "white",
         weight: 0.5,
+        // get color of circle based on earthquake depth
         fillColor: getColor(location.coordinates[2]),
-        // Adjust radius
-        radius: response[i].properties.mag * 12000
-      }).bindPopup("<h3>Magnitude: " + response[i].properties.mag 
+        // Adjust radius based on magnitude
+        radius: earthquakeData[i].properties.mag * 12000
+      }).bindPopup("<h3>Magnitude: " + earthquakeData[i].properties.mag 
         + "<br>Depth: " + location.coordinates[2] 
-        + " kms</h3><hr><strong>Location: </strong>" + response[i].properties.place 
-        + "<br><strong>Date: </strong>" + new Date(response[i].properties.time)).addTo(myMap);
+        + " kms</h3><hr><strong>Location: </strong>" + earthquakeData[i].properties.place 
+        + "<br><strong>Date: </strong>" + new Date(earthquakeData[i].properties.time)).addTo(myMap);
     }
   }
 
